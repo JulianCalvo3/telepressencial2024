78b638a15795466cace5f833c5300ed72fc83dfa
usuario <usuario@ubuntu-20.04-desktop-amd64.jesusamieiro.com>
2024-02-07 21:01:10 +0000

    feat-1: initial commit

---
+++ src/main/java/com/mycompany/holamundojava/Holamundojava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundojava;
+
+/**
+ *
+ * @author usuario
+ */
+public class Holamundojava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
